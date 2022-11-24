d6881ed2cd3b7578c89998b28e546f5fd35887fb
busy-gnl <vallantjesse@live.com>
2022-11-24 22:28:39 +0100

    dev reborn

--- database.sql
+++ database.sql
@@ -21,13 +21,13 @@ description TEXT(1000)
 );
 
 INSERT INTO van VALUES
-(1, 'roof_tent', false, false, false, false, false, 2, 1.9, 'assets/images/rooftent.jpg', 'link1', 'link2', 'link3', "Le moyen le plus simple et le moins cher pour partir à l'aventure!"),
-(2, 'combi', false, false, false, false, false, 3, 3, 'assets/images/combi.jpg', 'link1', 'link2', 'link3', "Synonyme de liberté et de voyage, le combi fait rêver les collectionneurs nostalgiques."),
-(3, 'simple_van', false, false, false, false, false, 3, 2.5, 'assets/images/simplevan.jpg', 'link1', 'link2', 'link3', "Pour ceux qui n'ont pas besoin de beaucoup de confort."),
-(4, 'camper_van', false, false, true, true, true, 3, 3, 'assets/images/campervan.jpg', 'link1', 'link2', 'link3', "Le bon compromis entre la voiture et le camping-car."),
-(5, 'fourgon', true, true, true, true, true, 5, 3.7, 'assets/images/fourgon.jped', 'link1', 'link2', 'link3', "Personnalisez le à votre goût."),
-(6, 'camping_car', true, true, true, true, true, 5, 6.5, 'assets/images/camping_car.jpg', 'link1', 'link2', 'link3', "Pour voyager tout comfort."),
-(7, 'adventurer', false, false, false, true, true, 3, 1.9, 'assets/images/adventurer.jpg', 'link1', 'link2', 'link3', "Pour explorer les zones géographiques reculées."),
-(8, 'tour_bus', true, true, true, true, true, 10, 1.9, 'assets/images/tourbus.jpg', 'link1', 'link2', 'link3', "Pour ceux qui ont vraiment besoin d'espace. ");
+(1, 'Tente de toît', false, false, false, false, false, 2, 1.9, 'assets/images/rooftent.jpg', 'link1', 'link2', 'link3', "Le moyen le plus simple et le moins cher pour partir à l'aventure!"),
+(2, 'Combi VW', false, false, false, false, false, 3, 3, 'assets/images/combi.jpg', 'link1', 'link2', 'link3', "Synonyme de liberté et de voyage, le combi fait rêver les collectionneurs nostalgiques."),
+(3, 'Van sans aménagement', false, false, false, false, false, 3, 2.5, 'assets/images/simplevan.jpg', 'link1', 'link2', 'link3', "Pour ceux qui n'ont pas besoin de beaucoup de confort."),
+(4, 'Van compact aménagé', false, false, true, true, true, 3, 3, 'assets/images/campervan.jpg', 'link1', 'link2', 'link3', "Le bon compromis entre la voiture et le camping-car."),
+(5, 'Fourgon aménagé', true, true, true, true, true, 5, 3.7, 'assets/images/fourgon.jped', 'link1', 'link2', 'link3', "Personnalisez le à votre goût."),
+(6, 'Camping car', true, true, true, true, true, 5, 6.5, 'assets/images/camping_car.jpg', 'link1', 'link2', 'link3', "Pour voyager tout comfort."),
+(7, 'Baroudeur', false, false, false, true, true, 3, 1.9, 'assets/images/adventurer.jpg', 'link1', 'link2', 'link3', "Pour explorer les zones géographiques reculées."),
+(8, 'Bus aménagé', true, true, true, true, true, 10, 1.9, 'assets/images/tourbus.jpg', 'link1', 'link2', 'link3', "Pour ceux qui ont vraiment besoin d'espace. ");
 
 SELECT * FROM van;
